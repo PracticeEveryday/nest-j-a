4e3f5a25a264a4f04925bbecb0cfb69efda54634
DongHyun Kim <developerforus@gmail.com>
2022-09-18 18:46:09 +0900

    user 생성 API

--- nestjs-test/src/auth/auth.repository.ts
+++ nestjs-test/src/auth/auth.repository.ts
@@ -1,6 +1,15 @@
 import { CustomRepository } from 'src/db/typeorm-ex.decorator';
 import { Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credential.dto';
 import { User } from './user.entity';
 
 @CustomRepository(User)
-export class UserRepository extends Repository<User> {}
+export class UserRepository extends Repository<User> {
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<User> {
+    const { username, password } = authCredentialsDto;
+    const user = this.create({ username, password });
+
+    await this.save(user);
+    return user;
+  }
+}
--- nestjs-test/src/auth/auth.service.ts
+++ nestjs-test/src/auth/auth.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { UserRepository } from './auth.repository';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialsDto } from './dto/auth-credential.dto';
+import { User } from './user.entity';
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserRepository) private userRepository: UserRepository,
   ) {}
+
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<User> {
+    return await this.userRepository.createUser(authCredentialsDto);
+  }
 }
