83ed2cd551829f82fb27b00793d1f55285a5cb76
DongHyun Kim <developerforus@gmail.com>
2022-09-18 18:58:13 +0900

    username 중복 에러 헨들링

--- nestjs-test/src/auth/auth.repository.ts
+++ nestjs-test/src/auth/auth.repository.ts
@@ -1,3 +1,7 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { CustomRepository } from 'src/db/typeorm-ex.decorator';
 import { Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
@@ -6,10 +10,18 @@ import { User } from './user.entity';
 @CustomRepository(User)
 export class UserRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<User> {
-    const { username, password } = authCredentialsDto;
-    const user = this.create({ username, password });
+    try {
+      const { username, password } = authCredentialsDto;
+      const user = this.create({ username, password });
 
-    await this.save(user);
-    return user;
+      await this.save(user);
+      return user;
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException(`Existing username`);
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
